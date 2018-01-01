6d5b042324ca2ba6cc135dbc2531348db726300b
ammisaidkaidi <ammi.said.kaidi@gmail.com>
2018-01-01 12:26:16 +0100

    Added files and changes required for deployment to heroku

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+var mongo = require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -42,5 +44,185 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+var mongoURL = 'mongodb://ammisaidkaidi:023942332as@ds239217.mlab.com:39217/qapp';
+
+function onDBError(stat, e) {
+    console.error("Error", stat + " : \r\n", stat);
+}
+
+function connect(url) {
+    mongo.MongoClient.connect(mongoURL, function (err, db) {
+        if (err) onDBError("connection", err);
+        console.log("Database created!");
+        db.close();
+    });
+}
+//var t = new MongoClient();
+//t.connect()
+//t.connect(mongoURL, { keepAlive: true }, (err, db) => {
+//    console.log(db.databaseName);
+//});
+
+function createCollection() {
+    mongo.connect(mongoURL, function (err, db) {
+        console.log("B");
+        if (err) throw err;
+        console.log("C");
+        /*
+         * First we'll add a few songs. Nothing is required to create the 
+         * songs collection; it is created automatically when we insert.
+         */
+        db.
+        console.log(db.collection);
+        var songs = db.collection('songs');
+        console.log("F");
+        // Note that the insert method can take either an array or a dict.
+
+        songs.insert(seedData, function (err, result) {
+
+            if (err) throw err;
+
+            /*
+             * Then we need to give Boyz II Men credit for their contribution
+             * to the hit "One Sweet Day".
+             */
+
+            songs.update(
+                { song: 'One Sweet Day' },
+                { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
+                function (err, result) {
+
+                    if (err) throw err;
+
+                    /*
+                     * Finally we run a query which returns all the hits that spend 10 or
+                     * more weeks at number 1.
+                     */
+
+                    songs.find({ weeksAtOne: { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+
+                        if (err) throw err;
+
+                        docs.forEach(function (doc) {
+                            console.log(
+                                'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
+                                ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+                            );
+                        });
+
+                        // Since this is an example, we'll clean up after ourselves.
+                        songs.drop(function (err) {
+                            if (err) throw err;
+
+                            // Only close the connection when your app is terminating.
+                            db.close(function (err) {
+                                if (err) throw err;
+                            });
+                        });
+                    });
+                }
+            );
+        });
+    });
+
+};
+try {
+    //connect();    
+    console.log("A");
+
+    var mongodb = require('mongodb');
+
+    // Create seed data
+
+    var seedData = [
+        {
+            decade: '1970s',
+            artist: 'Debby Boone',
+            song: 'You Light Up My Life',
+            weeksAtOne: 10
+        },
+        {
+            decade: '1980s',
+            artist: 'Olivia Newton-John',
+            song: 'Physical',
+            weeksAtOne: 10
+        },
+        {
+            decade: '1990s',
+            artist: 'Mariah Carey',
+            song: 'One Sweet Day',
+            weeksAtOne: 16
+        }
+    ];
+
+    // Standard URI format: mongodb://[dbuser:dbpassword@]host:port/dbname
+
+    var uri = mongoURL;
+
+    mongodb.MongoClient.connect(uri, function (err, db) {
+
+        if (err) throw err;
+
+        /*
+         * First we'll add a few songs. Nothing is required to create the 
+         * songs collection; it is created automatically when we insert.
+         */
+
+        var songs = db.collection('songs');
+
+        // Note that the insert method can take either an array or a dict.
+
+        songs.insert(seedData, function (err, result) {
+
+            if (err) throw err;
+
+            /*
+             * Then we need to give Boyz II Men credit for their contribution
+             * to the hit "One Sweet Day".
+             */
+
+            songs.update(
+                { song: 'One Sweet Day' },
+                { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
+                function (err, result) {
+
+                    if (err) throw err;
+
+                    /*
+                     * Finally we run a query which returns all the hits that spend 10 or
+                     * more weeks at number 1.
+                     */
+
+                    songs.find({ weeksAtOne: { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+
+                        if (err) throw err;
+
+                        docs.forEach(function (doc) {
+                            console.log(
+                                'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
+                                ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+                            );
+                        });
+
+                        // Since this is an example, we'll clean up after ourselves.
+                        songs.drop(function (err) {
+                            if (err) throw err;
+
+                            // Only close the connection when your app is terminating.
+                            db.close(function (err) {
+                                if (err) throw err;
+                            });
+                        });
+                    });
+                }
+            );
+        });
+    });
+
+    //createCollection();
+    
+} catch (e) {
+    console.log(e);
+}
 
 module.exports = app;
